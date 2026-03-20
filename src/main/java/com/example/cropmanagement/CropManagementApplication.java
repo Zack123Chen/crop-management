e1c83a165b40e7c0068822835db8daba95b49a28
Jack <jack@JackdeMacBook-Pro.local>
2026-03-20 15:19:18 +0800

    feat: 项目初始化完成

---
+++ src/main/java/com/example/cropmanagement/CropManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.cropmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CropManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cropmanagement/CropManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cropmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
